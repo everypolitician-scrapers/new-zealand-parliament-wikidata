b9920936bb5552b1df5176f40a562033620ca0d3
Tony Bowden <tony@tmtm.com>
2016-03-12 06:16:07 +0000

    Modernise scraper

--- scraper.rb
+++ scraper.rb
@@ -1,37 +1,22 @@
 #!/bin/env ruby
 # encoding: utf-8
 
-require 'rest-client'
-require 'scraperwiki'
 require 'wikidata/fetcher'
-require 'nokogiri'
-require 'colorize'
-require 'pry'
-require 'open-uri/cached'
-OpenURI::Cache.cache_path = '.cache'
 
+electorate = EveryPolitician::Wikidata.wikipedia_xpath( 
+  url: 'https://en.wikipedia.org/wiki/New_Zealand_general_election,_2014',
+  xpath: '//table[.//caption[contains(.,"Electorate results")]]//td[position() = last() - 5]//a[not(@class="new")]/@title',
+) 
 
-def noko_for(url)
-  Nokogiri::HTML(open(URI.escape(URI.unescape(url))).read) 
-end
+party = EveryPolitician::Wikidata.wikipedia_xpath( 
+  url: 'https://en.wikipedia.org/wiki/New_Zealand_general_election,_2014',
+  after: '//span[@id="List_results"]',
+  before: '//span[@id="Unsuccessful_list_candidates"]',
+  xpath: '//table[.//tr[1]//td[.="National"]]//tr[position() > 1]/td//a[not(@class="new")]/@title',
+) 
 
-def wikinames_from(url)
-  noko = noko_for(url)
-            # numrows => wanted
-  wantcol = { 8 => 2, 9 => 3, 10 => 4 }
-  electorate = noko.xpath('//table[caption[contains(.,"Electorate results")]]//tr[td[2]]').map { |tr|
-    tds = tr.css('td')
-    tds[wantcol[tds.count]]
-  }.map { |td| td.xpath('.//a[not(@class="new")]/@title').text }
+extras = [ 'Ria Bond', 'Maureen Pugh', 'Marama Davidson' ]
 
-  partylist = noko.xpath('//h3[span[@id="List_results"]]/following-sibling::table[1]//tr[3]//a[not(@class="new")]/@title').map(&:text)
-
-  hardcoded = [ 'Ria Bond' ]
-  return (electorate + partylist + hardcoded).uniq
-end
-
-names = wikinames_from('https://en.wikipedia.org/wiki/New_Zealand_general_election,_2014')
-EveryPolitician::Wikidata.scrape_wikidata(names: { en: names }, output: false)
-warn EveryPolitician::Wikidata.notify_rebuilder
+EveryPolitician::Wikidata.scrape_wikidata(names: { en: electorate | party | extras }, output: false)
 
 

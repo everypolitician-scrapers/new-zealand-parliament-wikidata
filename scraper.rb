53fc757ea388da31792576842932b99324e5404c
Tony Bowden <tony@tmtm.com>
2016-05-31 16:44:59 +0200

    Add Term 50

--- scraper.rb
+++ scraper.rb
@@ -3,20 +3,27 @@
 
 require 'wikidata/fetcher'
 
-electorate = EveryPolitician::Wikidata.wikipedia_xpath( 
+electorate = EveryPolitician::Wikidata.wikipedia_xpath(
   url: 'https://en.wikipedia.org/wiki/New_Zealand_general_election,_2014',
   xpath: '//table[.//caption[contains(.,"Electorate results")]]//td[position() = last() - 5]//a[not(@class="new")]/@title',
-) 
+)
 
-party = EveryPolitician::Wikidata.wikipedia_xpath( 
+party = EveryPolitician::Wikidata.wikipedia_xpath(
   url: 'https://en.wikipedia.org/wiki/New_Zealand_general_election,_2014',
   after: '//span[@id="List_results"]',
   before: '//span[@id="Unsuccessful_list_candidates"]',
   xpath: '//table[.//tr[1]//td[.="National"]]//tr[position() > 1]/td//a[not(@class="new")]/@title',
-) 
+)
+
+term_50 = EveryPolitician::Wikidata.wikipedia_xpath(
+  url: 'https://en.wikipedia.org/wiki/50th_New_Zealand_Parliament',
+  after: '//span[@id="Members"]',
+  before: '//span[@id="Parliamentary_business"]',
+  xpath: '//table//tr//td[2]//a[not(@class="new")]/@title',
+)
 
 extras = [ 'Ria Bond', 'Maureen Pugh', 'Marama Davidson' ]
 
-EveryPolitician::Wikidata.scrape_wikidata(names: { en: electorate | party | extras }, output: false)
+EveryPolitician::Wikidata.scrape_wikidata(names: { en: electorate | party | term_50 | extras }, output: false)
 
 
